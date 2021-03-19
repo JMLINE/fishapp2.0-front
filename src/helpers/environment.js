8d9a146527943747246f828b4d2497a492af01a0
Jason Line <jasonline87@gmail.com>
2021-03-19 18:08:39 -0400

    test

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -7,9 +7,9 @@ switch (window.location.hostname) {
         APIURL = 'http://localhost:4000/';
         break;
         // this is the deployed react application
-    case 'jl-fishingapp-two-front.herokuapp.com':
+    case 'jl-fishingapp-two.herokuapp.com':
         // this is the full url of your deployed API
-        APIURL = 'https://jl-fishingapp-two.herokuapp.com/'
+        APIURL = 'https://jl-fishingapp-two-front.herokuapp.com/'
 }
 
 export default APIURL;
